10383b7eaedacecc97fbc0ac8ae9ed9c11b57c87
Jamie Brandon <jamie@scattered-thoughts.net>
2020-04-11 14:05:43 -0700

    Can cross build!

---
+++ memory/build.zig
@@ -0,0 +1,59 @@
+const std = @import("std");
+const Builder = std.build.Builder;
+const builtin = @import("builtin");
+
+const str = []const u8;
+const strbuf = std.ArrayList(u8);
+const alloc = std.testing.allocator;
+
+pub fn build(b: *Builder) !void {
+
+    const mode = b.standardReleaseOptions();
+
+    const local = b.addExecutable("focus-local", "./src/main.zig");
+    include_common(local);
+    local.setBuildMode(mode);
+    local.install();
+
+    const cross = b.addExecutable("focus-cross", "./src/main.zig");
+    include_common(cross);
+    cross.setBuildMode(mode);
+    cross.setTarget(
+        std.zig.CrossTarget{
+            .cpu_arch = .aarch64,
+            .os_tag = .linux,
+            .abi = .musl,
+        }
+    );
+    cross.setOutputDir("zig-cache");
+    cross.install();
+
+    const local_run = local.run();
+    const local_step = b.step("local", "Run locally");
+    local_step.dependOn(&local_run.step);
+
+    const cross_step = b.step("cross", "Build for focus");
+    cross_step.dependOn(&cross.step);
+}
+
+fn include_common(exe: *std.build.LibExeObjStep) void {
+    exe.linkSystemLibrary("c");
+    exe.linkSystemLibrary("GL");
+    exe.linkSystemLibrary("GLU");
+    exe.linkSystemLibrary("glfw3");
+    exe.addIncludeDir("./include");
+    exe.addCSourceFile("./src/nk_main.c", &[_]str{
+        "-std=c99",
+        // nuklear causes undefined behaviour
+        // https://github.com/ziglang/zig/wiki/FAQ#why-do-i-get-illegal-instruction-when-using-with-zig-cc-to-build-c-code
+        "-fno-sanitize=undefined",
+    });
+}
+
+fn include_nix(exe: *std.build.LibExeObjStep, env_var: str) !void {
+    var buf = strbuf.init(alloc);
+    defer buf.deinit();
+    try buf.appendSlice(std.os.getenv(env_var).?);
+    try buf.appendSlice("/include");
+    exe.addIncludeDir(buf.items);
+}
---
+++ memory/default.nix
@@ -0,0 +1,40 @@
+let
+
+  pkgs = import <nixpkgs> {};
+
+  # crossPkgs = import <nixpkgs> {crossSystem = pkgs.lib.systems.examples.aarch64-multiplatform;};
+
+  hostPkgs = import <nixpkgs> {
+    system = "aarch64-linux";
+  };
+
+  crossPkgs = import <nixpkgs> {
+    overlays = [(self: super: {
+      inherit (hostPkgs)
+        mesa
+        libGL
+        libGLU
+        # glfw3
+      ;
+    })];
+    crossSystem = pkgs.lib.systems.examples.aarch64-multiplatform;
+  };
+
+in
+
+pkgs.stdenv.mkDerivation rec {
+  name = "memory";
+  buildInputs = let
+    inputs = pkgs: [
+      pkgs.libGL.all
+      pkgs.libGLU.all
+      pkgs.glfw3.all
+      # zig # tracking master instead
+    ];
+  in
+    [
+      pkgs.pkg-config
+    ] ++
+    # (inputs pkgs) ++
+    (inputs crossPkgs);
+}
