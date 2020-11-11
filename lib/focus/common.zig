0b09a58f95dfe4a7460b9e70b4e8045f8c220a6c
Jamie Brandon <jamie@scattered-thoughts.net>
2020-11-11 12:45:23 -0800

    Fix dump

--- lib/focus/common.zig
+++ lib/focus/common.zig
@@ -121,12 +121,11 @@ pub fn dumpInto(out_stream: anytype, indent: u32, thing: anytype) anyerror!void 
             if (uti.tag_type) |tag_type| {
                 try out_stream.writeAll(@typeName(@TypeOf(thing)));
                 try out_stream.writeAll("{\n");
-                inline for (uti.fields) |fti| {
-                    const field = fti.enum_field.?;
-                    if (@enumToInt(std.meta.activeTag(thing)) == field.value) {
+                inline for (@typeInfo(tag_type).Enum.fields) |fti| {
+                    if (@enumToInt(std.meta.activeTag(thing)) == fti.value) {
                         try out_stream.writeByteNTimes(' ', indent + 4);
-                        try std.fmt.format(out_stream, ".{} = ", .{field.name});
-                        try dumpInto(out_stream, indent + 4, @field(thing, field.name));
+                        try std.fmt.format(out_stream, ".{} = ", .{fti.name});
+                        try dumpInto(out_stream, indent + 4, @field(thing, fti.name));
                         try out_stream.writeAll("\n");
                         try out_stream.writeByteNTimes(' ', indent);
                         try out_stream.writeAll("}");
