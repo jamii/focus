ed2ca0ae962948b1756d1ddc01160efe96a253c6
Jamie Brandon <jamie@scattered-thoughts.net>
2020-11-19 12:16:51 -0800

    Bump nixos version

--- shell.nix
+++ shell.nix
@@ -3,9 +3,9 @@
 let
 
   nixpkgs = builtins.fetchTarball {
-    name = "nixos-20.03";
-    url = "https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz";
-    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
+    name = "nixos-20.09";
+    url = "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz";
+    sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
   };
 
   hostPkgs = import nixpkgs {};
