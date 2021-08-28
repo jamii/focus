97a4c7689520f53dffc0cb935c26b77141f10cbb
Jamie Brandon <jamie@scattered-thoughts.net>
2021-08-28 14:29:53 -0700

    Highlight imp code

--- lib/focus/language.zig
+++ lib/focus/language.zig
@@ -2,6 +2,7 @@ const focus = @import("../focus.zig");
 usingnamespace focus.common;
 const style = focus.style;
 const meta = focus.meta;
+const imp = @import("../../imp/lib/imp.zig");
 
 pub const Language = enum {
     Zig,
@@ -41,10 +42,55 @@ pub const Language = enum {
     pub fn highlight(self: Language, allocator: *Allocator, source: []const u8, range: [2]usize) []const Color {
         _ = source;
         const colors = allocator.alloc(Color, range[1] - range[0]) catch oom();
+        for (colors) |*color| color.* = style.text_color;
         switch (self) {
-            else => {
-                for (colors) |*color, i| color.* = style.text_color;
+            .Imp => {
+                var arena = ArenaAllocator.init(allocator);
+                defer arena.deinit();
+                var store = imp.lang.Store.init(&arena);
+                var error_info: ?imp.lang.pass.parse.ErrorInfo = null;
+                var parser = imp.lang.pass.parse.Parser{
+                    .store = &store,
+                    .source = source[range[0]..range[1]],
+                    .position = 0,
+                    .error_info = &error_info,
+                };
+                while (true) {
+                    const start = parser.position;
+                    if (parser.nextTokenMaybe()) |maybe_token| {
+                        if (maybe_token) |token| {
+                            switch (token) {
+                                .EOF => break,
+                                .None, .Some, .Number, .Text, .Name, .When, .Fix, .Reduce, .Enumerate => {
+                                    const hash = meta.deepHash(source[start..parser.position]);
+                                    const color = Color{
+                                        .r = @intCast(u8, 192 + (meta.deepHash([2]u64{ hash, 0 }) % 64)),
+                                        .g = @intCast(u8, 192 + (meta.deepHash([2]u64{ hash, 1 }) % 64)),
+                                        .b = @intCast(u8, 192 + (meta.deepHash([2]u64{ hash, 2 }) % 64)),
+                                        .a = 255,
+                                    };
+                                    var i: usize = start;
+                                    while (i < parser.position) : (i += 1)
+                                        colors[i] = color;
+                                },
+                                else => {
+                                    var i: usize = start;
+                                    while (i < parser.position) : (i += 1)
+                                        colors[i] = style.keyword_color;
+                                },
+                            }
+                        } else {
+                            var i: usize = start;
+                            while (i < parser.position) : (i += 1)
+                                colors[i] = style.comment_color;
+                        }
+                    } else |err| {
+                        if (err == error.OutOfMemory) oom();
+                        parser.position += 1;
+                    }
+                }
             },
+            else => {},
         }
         return colors;
     }
--- lib/focus/style.zig
+++ lib/focus/style.zig
@@ -5,6 +5,8 @@ pub const background_color = Color{ .r = 0x2e, .g = 0x34, .b = 0x36, .a = 255 };
 pub const fade_color = Color{ .r = 0x2e, .g = 0x34, .b = 0x36, .a = 100 };
 pub const status_background_color = Color{ .r = @divTrunc(0x2e, 2), .g = @divTrunc(0x34, 2), .b = @divTrunc(0x36, 2), .a = 255 };
 pub const text_color = Color{ .r = 0xee, .g = 0xee, .b = 0xec, .a = 255 };
+pub const keyword_color = Color{ .r = 0xff, .g = 0xff, .b = 0xff, .a = 255 };
+pub const comment_color = Color{ .r = 0xaa, .g = 0xaa, .b = 0xaa, .a = 255 };
 pub const error_text_color = Color{ .r = 0xff, .g = 0x00, .b = 0x00, .a = 255 };
 pub const highlight_color = Color{ .r = 0xee, .g = 0xee, .b = 0xec, .a = 100 };
 pub const multi_cursor_color = Color{ .r = 0x7a, .g = 0xa6, .b = 0xda, .a = 255 };
