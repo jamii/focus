5ddae03f14ffd687b49c11680479c1a5819fcbd6
Jamie Brandon <jamie@scattered-thoughts.net>
2021-12-12 10:12:32 -0800

    Bump zig version

--- shell.nix
+++ shell.nix
@@ -28,11 +28,11 @@ let
   zig = hostPkgs.stdenv.mkDerivation {
         name = "zig";
         src = fetchTarball (if (hostPkgs.system == "x86_64-linux") then {
-            url = "https://ziglang.org/builds/zig-linux-x86_64-0.9.0-dev.1625+d3a099c14.tar.xz";
-            sha256 = "1azv16jkn7mh1jm9m6k7d5nkjjs26dhy26azg9xcb4hg5qrznnc0";
+            url = "https://ziglang.org/builds/zig-linux-x86_64-0.9.0-dev.1801+a4aff36fb.tar.xz";
+            sha256 = "12ir9nqf45rw7kiv4yn6m9zbacg1svz16fl69ibjrqc9nkd6p047";
         } else if (hostPkgs.system == "aarch64-linux") then {
-        url = "https://ziglang.org/builds/zig-linux-aarch64-0.9.0-dev.1801+a4aff36fb.tar.xz";
-        sha256 = "1sbkci9rs8yjvbbl6szy3hz1ihkjvcb41w6hnzlkf3p1zhc7y43i";
+            url = "https://ziglang.org/builds/zig-linux-aarch64-0.9.0-dev.1801+a4aff36fb.tar.xz";
+            sha256 = "1sbkci9rs8yjvbbl6szy3hz1ihkjvcb41w6hnzlkf3p1zhc7y43i";
     } else throw ("Unknown system " ++ hostPkgs.system));
         dontConfigure = true;
         dontBuild = true;
