ef3bcb10d5a8fb74613c2377bbf63489ae6002a0
Jamie Brandon <jamie@scattered-thoughts.net>
2023-08-04 13:36:46 -0700

    Remove flake. Bump to zig 0.11.0.

--- shell.nix
+++ shell.nix
@@ -1,12 +1,52 @@
-(import
-  (
-    let
-      flake-compat = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.flake-compat;
-    in
-      fetchTarball {
-        url = "https://github.com/edolstra/flake-compat/archive/${flake-compat.locked.rev}.tar.gz";
-        sha256 = flake-compat.locked.narHash;
-      }
-  )
-  {src = ./.;})
-.shellNix
+{ cross ? false }:
+
+let
+
+  hostPkgs = import <nixpkgs> {};
+
+  armPkgs = import <nixpkgs> {
+    system = "aarch64-linux";
+  };
+
+  crossPkgs = import <nixpkgs> {
+    overlays = [(self: super: {
+      inherit (armPkgs)
+        gcc
+        mesa
+        libGL
+      ;
+    })];
+    crossSystem = hostPkgs.lib.systems.examples.aarch64-multiplatform;
+  };
+
+  targetPkgs = if cross then crossPkgs else hostPkgs;
+
+  zig = hostPkgs.stdenv.mkDerivation {
+        name = "zig";
+        src = fetchTarball (
+            if (targetPkgs.system == "x86_64-linux") then {
+                url = "https://ziglang.org/download/0.11.0/zig-linux-x86_64-0.11.0.tar.xz";
+                sha256 = "0385m6sfaxcfy91l4iwi3zkr705zbn4basvkkkbba7yh635aqr78";
+            } else
+            throw ("Unknown system " ++ targetPkgs.system)
+        );
+        dontConfigure = true;
+        dontBuild = true;
+        installPhase = ''
+            mkdir -p $out
+            mv ./* $out/
+            mkdir -p $out/bin
+            mv $out/zig $out/bin
+        '';
+    };
+
+in
+
+hostPkgs.mkShell rec {
+  buildInputs = [
+    zig
+    hostPkgs.git
+    targetPkgs.libGL.all
+    targetPkgs.xorg.libX11.dev
+  ];
+}
