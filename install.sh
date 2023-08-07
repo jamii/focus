d6d2d6d97c26bccb688858e7bb3f7f5df45bb0d0
Jamie Brandon <jamie@scattered-thoughts.net>
2023-08-07 12:31:36 -0700

    Install from zig-out instead of zig-cache

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 
 nix-shell --run 'zig build install -Doptimize=ReleaseSafe'
 pkill -9 focus; sleep 1
-cp ./zig-cache/focus-dev ~/bin/focus
+cp ~/bin/focus "~/bin/focus-$(date +%Y-%m-%d_%H-%M-%S)"
+cp ./zig-out/bin/focus-dev ~/bin/focus
+cp ./zig-out/bin/focus-dev ~/bin/focus-root
