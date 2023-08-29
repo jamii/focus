d7dc3da79517355f3c5fc8104795855613a2250d
Jamie Brandon <jamie@scattered-thoughts.net>
2023-08-29 14:36:24 -0700

    Improve install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 nix-shell --run 'zig build install -Doptimize=ReleaseSafe'
-pkill -9 focus; sleep 1
-cp ~/bin/focus "~/bin/focus-$(date +%Y-%m-%d_%H-%M-%S)"
+sudo pkill -9 focus; sleep 1
+cp ~/bin/focus "/home/jamie/bin/focus-$(date +%Y-%m-%d_%H-%M-%S)"
 cp ./zig-out/bin/focus-dev ~/bin/focus
 cp ./zig-out/bin/focus-dev ~/bin/focus-root
